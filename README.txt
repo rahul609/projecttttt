42d57d21014c064d6c6f41a04f7aba448fbf7488
jagjeet0824 <jagjeet0824@gmail.com>
2018-04-24 05:16:29 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jQueryCards/pen/dec4d80a351e9a03c752d85db300b162.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,15 @@
+$('#tabs').tabulous({
+    	effect: 'scale'
+    });
+
+     $('#tabs2').tabulous({
+    	effect: 'slideLeft'
+    });
+
+     $('#tabs3').tabulous({
+    	effect: 'scaleUp'
+    });
+
+    $('#tabs4').tabulous({
+    	effect: 'flip'
+    });
